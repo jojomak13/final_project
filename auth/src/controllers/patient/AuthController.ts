8c72f5403d217ab82e3ee257d55af9f9ef833f27
youssef-99 <33738946+youssef-99@users.noreply.github.com>
2021-03-10 01:59:20 +0200

    signup controller

--- auth/src/controllers/patient/AuthController.ts
+++ auth/src/controllers/patient/AuthController.ts
@@ -1,14 +1,18 @@
+import { BadRequestError } from '@hti/common';
 import { Request, Response } from 'express';
-import { Patient } from '../../models/Patient';
+import { Patient } from '../../models/patient';
 
-export const signup = async (data: Object, req: Request, res: Response) => {
+
+export const signup = async (data: any, req: Request, res: Response) => {
   console.log(data);
-  res.json('done');
-  // const patient = await Patient.findOne({ email: req.email });
-  // if (patient) {
-  // TODO
-  // throw error already exsist
-  // }
-  // TODO
-  // make new patient
+
+  const patient = await Patient.findOne({ email: data.email, phone: data.phone });
+  if (patient) {
+    throw new BadRequestError('email already exist', req.statusCode);
+  }
+
+  const newPatient = Patient.build(data);
+  await newPatient.save();
+
+  return newPatient;
 };
--- auth/src/requests/patient/SignupRequest.ts
+++ auth/src/requests/patient/SignupRequest.ts
@@ -1,7 +1,7 @@
 import Joi from 'joi';
 
 const SignupRequest = Joi.object({
-  username: Joi.string().alphanum().min(3).max(30).required(),
+  name: Joi.string().alphanum().min(3).max(30).required(),
 
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
 
