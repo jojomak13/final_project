28542e616c28b9d43a4710ff65b94a53c4eb5d5a
youssef-99 <33738946+youssef-99@users.noreply.github.com>
2021-03-11 22:55:14 +0200

    basic info and about me request validation

---
+++ auth/src/requests/doctor/profile/AboutmeRequest.ts
@@ -0,0 +1,31 @@
+import Joi from 'joi';
+
+const AboutmeRequest = Joi.object({
+    title: Joi.array().items(
+        Joi.object({
+            lang: Joi.string(),
+            value: Joi.string()
+        })
+    ),
+    biography: Joi.array().items(
+        Joi.object({
+            lang: Joi.string(),
+            value: Joi.string()
+        })
+    ),
+    prefix: Joi.array().items(
+        Joi.object({
+            lang: Joi.string(),
+            value: Joi.string()
+        })
+    ),
+    languages: Joi.array().items(
+        Joi.object({
+            lang: Joi.string(),
+            value: Joi.string()
+        })
+    ),
+
+});
+
+export { AboutmeRequest };
