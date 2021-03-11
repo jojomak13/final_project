5e51eda43f483950654177244bd614cc758a3eb4
youssef-99 <33738946+youssef-99@users.noreply.github.com>
2021-03-11 20:26:35 +0200

    doctor auth controller

--- auth/src/controllers/doctor/authController.ts
+++ auth/src/controllers/doctor/authController.ts
@@ -1,3 +1,52 @@
+import { BadRequestError, DBValidationError } from '@hti/common';
 import { Request, Response } from 'express';
+import { Auth } from '../../helpers/Auth';
+import { Password } from '../../helpers/password';
+import { Doctor } from '../../models/Doctor';
 
-const signup = async (data: any, req: Request, res: Response) => {};
+export const signup = async (data: any, req: Request, res: Response) => {
+    const doctor = await Doctor.findOne().or([
+        { email: data.email },
+        { phone: data.phone },
+    ]);
+
+    if (doctor) {
+        const errors = [];
+
+        if (data.email === doctor.email)
+            errors.push({ message: 'email already exist', field: 'email' });
+        if (data.phone === doctor.phone)
+            errors.push({ message: 'phone already exist', field: 'phone' });
+
+        throw new DBValidationError(errors);
+    }
+
+    const newDoctor = Doctor.build(data);
+    await newDoctor.save();
+
+    return res.status(201).json({
+        status: true,
+        msg: 'acount created successfully',
+    });
+};
+
+export const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const doctor = await Doctor.findOne({ email }).populate('country');
+
+    if (!doctor || !(await Password.compare(password, doctor.password))) {
+        throw new BadRequestError('Invalid Credentials');
+    }
+
+    const token = await Auth.login(Doctor);
+
+    res.json({ status: true, data: { token } });
+};
+
+export const me = async (req: Request, res: Response) => {
+    return res.json({
+        status: true,
+        data: req.user,
+    });
+};
