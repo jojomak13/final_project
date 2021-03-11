bac99b45c02fc686c0b8223f940db3d8765be713
jojomak13 <jojomak350@gmail.com>
2021-03-11 20:45:48 +0200

    finsih all of auth for doctor

--- auth/src/routes/doctor/auth.ts
+++ auth/src/routes/doctor/auth.ts
@@ -1,20 +1,20 @@
-import { RequestValidationError } from '@hti/common';
+import { auth, RequestValidationError } from '@hti/common';
 import { Router, Request, Response } from 'express';
 import { SignupRequest } from '../../requests/doctor/SignupRequest';
 import * as AuthController from '../../controllers/doctor/AuthController';
+import { LoginRequest } from '../../requests/doctor/LoginRequest';
 
 const router = Router();
 
-router.get('/me', (req, res) => {
-  res.send('current doctor sent');
-});
+router.post('/login', async (req, res) => {
+  LoginRequest.validateAsync(req.body, {
+    abortEarly: false,
+    stripUnknown: true,
+  }).catch((err) => {
+    throw new RequestValidationError(err);
+  });
 
-router.post('/logout', (req, res) => {
-  res.send('loged out');
-});
-
-router.post('/login', (req, res) => {
-  res.send('doctor login');
+  await AuthController.login(req, res);
 });
 
 router.post('/register', async (req: Request, res: Response) => {
@@ -27,5 +27,7 @@ router.post('/register', async (req: Request, res: Response) => {
 
   await AuthController.signup(data, req, res);
 });
+
+router.get('/me', auth, AuthController.me);
 
 export { router as authRouter };
