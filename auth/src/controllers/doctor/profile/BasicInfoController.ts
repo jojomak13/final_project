4da32f5aae843a7c54b55c44ff80f0e9e7366c86
youssef-99 <33738946+youssef-99@users.noreply.github.com>
2021-03-11 21:49:02 +0200

    about me and basic info controllers

---
+++ auth/src/controllers/doctor/profile/BasicInfoController.ts
@@ -0,0 +1,34 @@
+import { BadRequestError } from "@hti/common";
+import { Request, Response } from "express";
+import { Password } from "../../../helpers/password";
+import { Doctor } from "../../../models/Doctor";
+
+export const edit = async (req: Request, res: Response) => {
+    const doctorBasicInfo = await Doctor.findById(req.user?.id).populate('country')
+        .select(["name", "email", "gender", "country", "phone", "date_of_birth", "job"]);
+
+    if (!doctorBasicInfo) {
+        throw new BadRequestError('oops, somthing went wrong');
+    }
+
+    res.json({ status: true, data: doctorBasicInfo });
+};
+
+export const update = async (data: any, req: Request, res: Response) => {
+    const doctorBasicInfo = await Doctor.findById(req.user?.id);
+
+    if (!doctorBasicInfo) {
+        throw new BadRequestError('oops, somthing went wrong');
+    }
+
+    if (data.password) {
+        data = {
+            ...data,
+            password: await Password.hash(data.password),
+        };
+    }
+
+    await Doctor.updateOne(data);
+
+    res.json({ status: true, msg: 'basic info updated successfully' })
+};
