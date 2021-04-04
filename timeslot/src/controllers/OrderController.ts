1d21c46a47c9b17105a906904f3e2f4555cb047e
youssef-99 <youssefwilliam970@gmail.com>
2021-04-04 03:07:14 +0200

    reschdule order controller

--- timeslot/src/controllers/OrderController.ts
+++ timeslot/src/controllers/OrderController.ts
@@ -44,8 +44,69 @@ export const store = async (data: any, req: Request, res: Response) => {
   });
 };
 
-export const schedule = async (req: Request, res: Response) => {
-  //
+export const reschedule = async (data: any,req: Request, res: Response) => {
+
+  //create new order
+  const timeslot = await Timeslot.findById(data.timeslot);
+  const patient = await Patient.findById(req.user?.id);
+
+  if (!timeslot) throw new NotFoundError();
+
+  if (timeslot.is_booked) {
+    throw new BadRequestError('This time slot already reserved before');
+  }
+
+  if (!patient) {
+    throw new Error(
+      `patient [${req.user?.id}] not found inside timeslot service`
+    );
+  }
+
+  const expiration = new Date();
+  expiration.setSeconds(
+    expiration.getSeconds() + parseInt(process.env.ORDER_EXPIRED_PERIOD!)
+  );
+
+  timeslot.set({ is_booked: true });
+
+  const newOrder = Order.build({
+    timeslot,
+    patient,
+    type: data.type,
+    status: OrderStatus.AwaitPayment,
+    expires_at: expiration,
+  });
+
+
+  // cancel old order
+  const oldOrder = await Order.findById(req.params.id).populate('timeslot');
+
+  if (!oldOrder || oldOrder.patient.toString() !== req.user?.id) {
+    throw new NotFoundError();
+  }
+
+  if (oldOrder.status === OrderStatus.Canelled) {
+    throw new BadRequestError('this order already cancelled before');
+  }
+
+  oldOrder.set({ status: OrderStatus.Canelled });
+  oldOrder.timeslot.set({ is_booked: false });
+
+  // save new order data
+  await newOrder.save();
+  await timeslot.save();
+
+  // save old order cancelation
+  await oldOrder.save();
+  await oldOrder.timeslot.save();
+
+  // TODO:: created refund logic here for reschedule
+
+  return res.json({
+    status: true,
+    msg: 'order rescheduled successfully',
+  }); 
+
 };
 
 export const cancel = async (req: Request, res: Response) => {
@@ -65,7 +126,7 @@ export const cancel = async (req: Request, res: Response) => {
   await order.save();
   await order.timeslot.save();
 
-  // TODO:: created refund logic here
+  // TODO:: created refund logic here dor canacel
 
   return res.json({
     status: true,
--- timeslot/src/routes/order.ts
+++ timeslot/src/routes/order.ts
@@ -1,11 +1,22 @@
 import { RequestValidationError } from '@hti/common';
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 import * as OrderController from '../controllers/OrderController';
 import { StoreOrderReruest } from '../requests/patient/StoreOrderRequest';
 
 const router = Router();
 
-router.post('/', async (req, res) => {
+router.post('/', async (req: Request, res: Response) => {
+  const data = await StoreOrderReruest.validateAsync(req.body, {
+    abortEarly: false,
+    stripUnknown: true,
+  }).catch((err) => { 
+    throw new RequestValidationError(err);
+  });
+
+  await OrderController.store(data, req, res);
+});
+
+router.patch('/reschedule/:id', async (req: Request, res: Response) => {
   const data = await StoreOrderReruest.validateAsync(req.body, {
     abortEarly: false,
     stripUnknown: true,
@@ -13,8 +24,9 @@ router.post('/', async (req, res) => {
     throw new RequestValidationError(err);
   });
 
-  await OrderController.store(data, req, res);
+  await OrderController.reschedule(data, req, res);
 });
+
 
 router.delete('/:id', OrderController.cancel);
 
